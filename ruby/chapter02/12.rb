359efe1b5f9a4ddf5be9285f067b3d3deb4d902e
Ayumu Mochizuki <200504@MBPR0201.local>
2018-05-11 17:33:56 +0900

    Add: 12.rb

---
+++ ruby/chapter02/12.rb
@@ -0,0 +1,7 @@
+f = File.open("hightemp_w.txt","r")
+col1 = []
+col2 = []
+f.each_line{|line| line = line.split(" "); col1 << line[0];col2 << line[1]}
+
+File.open("col1.txt","w"){|f| col1.each{|s| f.write(s + "\n");}}
+File.open("col2.txt","w"){|f| col2.each{|s| f.write(s + "\n");}}
