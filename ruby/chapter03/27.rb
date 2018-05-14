3277bad75fb284cb7a2d88fed0374fb7a2799332
Ayumu Mochizuki <200504@MBPR0201.local>
2018-05-14 16:51:37 +0900

    Fix: puts

--- ruby/chapter03/27.rb
+++ ruby/chapter03/27.rb
@@ -17,17 +17,15 @@ jsons.each { |json|
             next if val == nil
             val = val.gsub("'","")
             if val.match(/\[/)
-                puts val
                 m = val[/\[{2,2}*\|{1,}(.*?)\]{2,2}/,1]
                 t = m[/\|(.*?)$/,1] if m != nil
                 m = t if t != nil
                 val.gsub!(/\[{2,2}.*?\]{2,2}/,m) if m != nil
                 val.gsub!(/\[|\]/,"")
-                puts val
             end
             key = line[/\|(.*?)\=/,1]
             temp[key] = val if key != nil
         }
     end
 }
-# puts temp
+puts temp
